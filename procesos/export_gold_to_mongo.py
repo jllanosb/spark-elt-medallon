fe0e2a0def2fa8532cf49bb6b146162a5a9f449b
Jaime Llanos <jllanosb@unc.edu.pe>
2026-02-21 19:56:44 -0500

    Cargando Archivo exportar csv a mongodb

---
+++ procesos/export_gold_to_mongo.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+
+# Crear sesión Spark
+spark = SparkSession.builder \
+    .appName("CSV_to_MongoDB_Gold") \
+    .config("spark.mongodb.connection.uri", "mongodb://192.168.0.11:27017") \
+    .config("spark.mongodb.database", "medallon") \
+    .config("spark.mongodb.collection", "gold") \
+    .getOrCreate()
+
+# Leer CSV
+df = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("file:/home/hadoop/spark-elt-medallon/datalake/gold.csv")
+
+# Mostrar esquema
+df.printSchema()
+
+# Escribir en MongoDB
+df.write \
+    .format("mongodb") \
+    .mode("overwrite") \
+    .save()
+
+spark.stop()
